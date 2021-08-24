48a98c1cc9e5bef6a461b4604c21938dd3def965
Roberto Elias Patricio Rocha <relias.rocha@gmail.com>
2021-08-24 19:13:42 +0100

    Created Console App for Presentation and Service

---
+++ RecipeApp.Presentation.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RecipeApp.Presentation.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
